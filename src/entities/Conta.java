f176c5c12a259d8a089029d687661041acd95e66
oFerrari <andrei.domingos@estudante.ifms.edu.br>
2024-03-19 20:48:05 -0400

    Criando primeiro teste

---
+++ src/entities/Conta.java
@@ -0,0 +1,50 @@
+package entities;
+
+public class Conta {
+	public static double TAXA_DE_SERVICO = 0.02;
+	private Long id;
+	private Double saldo;
+	
+	public Conta(){
+		
+	}
+
+	public Conta(Long id, Double saldo) {
+		this.id = id;
+		this.saldo = saldo;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Double getSaldo() {
+		return saldo;
+	}
+	
+	
+	public void deposito(double valor) {
+		if(valor > 0) {
+			this.saldo -= valor * TAXA_DE_SERVICO;
+			this.saldo += this.saldo + valor;			
+		}
+	}
+	
+	public void sacar(double valor) {
+		if(valor > saldo) {
+			throw new IllegalArgumentException();
+		}
+		this.saldo -= valor;
+	}
+	
+	public double saqueValorTotal(double valor) {
+		double valorTotal = this.saldo;
+		this.saldo = 0.0;
+		return valorTotal;
+	}
+	
+}
---
+++ src/testes/entities/ContaTestes.java
@@ -0,0 +1,18 @@
+package testes.entities;
+
+public class ContaTestes {
+	/*
+	Nomenclatura de um teste
+	
+	<AÇÃO> deveria <EFEITO> [quando CENÁRIO]
+	*/
+	
+	/*
+	Quando o deposito ocorrer o saldo devera ser atualizado em positivo, e a taxa de serviço recolhida. 
+	*/
+	
+	//AÇÃO  DEVERIA EFEITO
+	depositoDeveriaAumentarSaldoEDescontarTaxaQuandoValorForPositivo(){
+		
+	}
+}
